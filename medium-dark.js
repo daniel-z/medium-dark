797d3c71104b08f693c46629dfbed6be82b21f69
Daniel Zamorano <dzamorano@tacitknowledge.com>
2019-10-31 20:29:22 -0600

    Initial version working for articles.

---
+++ medium-dark.js
@@ -0,0 +1,60 @@
+// Copyright 2019 Daniel Zamorano / danielzamorano.pro / <daniel.zamorano.m@gmail.com>
+// All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+function applyToElems(arrayOfElems, rule) {
+  for (var idx = 0; idx < arrayOfElems.length; idx++) {
+    arrayOfElems[idx].style = rule;
+  }
+}
+
+function applyDarkStyles () {
+  // theme colors
+  var darkColor = '#1c1c1c';
+  var lightColor = '#d1d2d3';
+  var grayColor = '#090909';
+
+  //  BG Color Elements
+  var bgColorClasses = ['c', 'dc'];
+  var bgstyle = `background-color: ${darkColor};`;
+
+  bgColorClasses.forEach(function changingColorElements(className) {
+    applyToElems(document.getElementsByClassName(className), bgstyle);
+  });
+
+  //  Light Color Elements
+  var textColorElementTagNames = [
+    'p',
+    'h1',
+    'h2',
+    'h3',
+    'h4',
+    'h5',
+    'h6',
+    'a',
+    'button',
+    'span',
+    'figcaption',
+    'li'
+  ];
+
+  var colorStyle = `color: ${lightColor} !important; fill:${lightColor} !important;`;
+
+  textColorElementTagNames.forEach(function changingColorElements(tagName) {
+    applyToElems(document.getElementsByTagName(tagName), colorStyle);
+  });
+
+  // special cases
+  applyToElems(document.getElementsByClassName('ax'), colorStyle);
+
+  // marks
+  applyToElems(document.getElementsByTagName('mark'), `background-color: ${grayColor};`);  
+};
+
+if (document.querySelector('head meta[property="al:ios:app_name"][content="medium" i]')) {
+  applyDarkStyles();
+	observer.observe(document.body, config);
+}
---
+++ options.js
@@ -0,0 +1,12 @@
+// Copyright 2018 Daniel Zamorano / danielzamorano.pro / <daniel.zamorano.m@gmail.com>
+// All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+function constructOptions() {
+  console.info("No options yet, working on it");
+}
+
+constructOptions();
